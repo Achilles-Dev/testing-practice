50265d33b5d756746aae4c1a5bf4c4b0e480e649
Solomon Achilles <solohag92@gmail.com>
2022-03-29 18:07:13 +0000

    Add task3.js | test calculator function

---
+++ task3.js
@@ -0,0 +1,21 @@
+class Calculator {
+  add(num1, num2){
+    return (num1 + num2);
+  }
+
+  subtract(num1, num2) {
+    return (num1 - num2);
+  }
+
+  multiply(num1, num2) {
+    return (num1 * num2);
+  }
+
+  divide(num1, num2) {
+    return (num1 / num2);
+  }
+}
+
+const calculator = new Calculator();
+
+module.exports = calculator;
--- tasks.test.js
+++ tasks.test.js
@@ -1,7 +1,9 @@
-const { it, expect } = require('@jest/globals');
+const { test, expect } = require('@jest/globals');
 const stringLength = require('./task1');
 
 const reverseString = require('./task2');
+
+const calculator = require('./task3');
 
 //String Length
 it('Length of string', () => {
@@ -23,4 +25,50 @@ it('Reverse String', () => {
 
 it('Reverse String', () => {
   expect(reverseString('The is a very long string')).toBe('gnirts gnol yrev a si eht');
+});
+
+//Calculator
+
+describe('add', () => {
+	test('adds 0 and 0', () => {
+		expect(calculator.add(0,0)).toBe(0);
+	});
+
+	test('adds 2 and 2', () => {
+		expect(calculator.add(2,2)).toBe(4);
+	});
+
+	test('adds positive numbers', () => {
+		expect(calculator.add(2,6)).toBe(8);
+	});
+});
+
+describe('subtract', () => {
+	test('subtracts numbers', () => {
+		expect(calculator.subtract(10,4)).toBe(6);
+	});
+
+  test('subtracts 0 from 1', () => {
+    expect(calculator.subtract(1,0)).toBe(1);
+  })
+});
+
+describe('multiply', () => {
+	test('multiplies two numbers', () => {
+		expect(calculator.multiply(2, 4)).toBe(8);
+	});
+
+	test('multiplies number and 0', () => {
+		expect(calculator.multiply(5, 0)).toBe(0);
+	});
+});
+
+describe('divide', () => {
+	test('divides two numbers', () => {
+		expect(calculator.divide(32, 4)).toBe(8);
+	});
+
+	test('divides 0 by number', () => {
+		expect(calculator.divide(0, 5)).toBe(0);
+	});
 });
